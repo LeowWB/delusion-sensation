e880226795efba875e075af03b4954819ad94893
Leow Wen Bin <dleowwb@gmail.com>
2019-06-04 15:31:36 +0800

    Implement basic functionality

--- src/main.rb
+++ src/main.rb
@@ -2,19 +2,36 @@ require 'byebug'
 
 # frozen_string_literal: true
 
+raw_text = []
+filter_list = []
+output = ""
+
 puts "Raw file location: "
 raw_file = File.open gets.chomp, "r"
+
+raw_file.each do |l|
+  raw_text.append l.downcase.split(/\W+/)
+end
+
+raw_file.close
+raw_text.flatten!
 
 puts "Filter file location: "
 filter_file = File.open gets.chomp, "r"
 
-puts "Output file location: "
-out_file = File.open gets.chomp, "w"
-
-filter_list = []
-
 filter_file.each do |l|
-  filter_list << l
+  filter_list << l.chomp
 end
 
+filter_file.close
+
 filter_list.sort!
+
+raw_text.select! do |w|
+  not filter_list.include? w
+end
+
+puts "Output file location: "
+out_file = File.open gets.chomp, "w"
+out_file.write raw_text.join(" ")
+out_file.close
