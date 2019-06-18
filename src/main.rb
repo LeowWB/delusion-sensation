978b66842b3cb5e889936ccb3e1031ab6126c7d8
Leow Wen Bin <dleowwb@gmail.com>
2019-06-18 17:07:32 +0800

    Refactor

--- src/main.rb
+++ src/main.rb
@@ -5,13 +5,11 @@ require './processor.rb'
 include Processor
 
 def main
+  raw_file_name, filter_file_name, out_file_name = get_file_names
+
   raw_text_list = []
-  filter_list = []
-  output = ""
-
-  puts "Raw file location: "
-  raw_file = File.open gets.chomp, "r"
-
+  raw_file = File.open raw_file_name, "r"
+  
   raw_file.each do |l|
     raw_text_list.append l.downcase.split(/[^\w$&-]+/)
   end
@@ -19,23 +17,37 @@ def main
   raw_file.close
   raw_text_list.flatten!
 
-  puts "Filter file location: "
-  filter_file = File.open gets.chomp, "r"
+
+  filter_list = []  
+  filter_file = File.open filter_file_name, "r"
 
   filter_file.each do |l|
     filter_list << l.chomp
   end
 
   filter_file.close
-
   filter_list.sort!
 
   raw_text_list = process_list raw_text_list, filter_list
+  raw_text = raw_text_list.join " "
 
-  puts "Output file location: "
-  out_file = File.open gets.chomp, "w"
-  out_file.write raw_text_list.join(" ")
+  output = ""
+  out_file = File.open out_file_name, "w"
+  out_file.write raw_text
   out_file.close
+end
+
+def get_file_names
+  puts "Raw file: "
+  raw_file_name = gets.chomp
+
+  puts "Filter file: "
+  filter_file_name = gets.chomp
+  
+  puts "Output file: "
+  out_file_name = gets.chomp
+  
+  return [raw_file_name, filter_file_name, out_file_name]
 end
 
 main
